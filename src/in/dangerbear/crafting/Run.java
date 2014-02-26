a443ec77326db9e874b9cbd903e957064e9223cb
Zhenya Frolov <hellcomestotown@hotmail.com>
2014-02-26 12:31:38 -0800

    Changed filepath to a static final.
    Better design, IMO.

--- src/in/dangerbear/crafting/Run.java
+++ src/in/dangerbear/crafting/Run.java
@@ -7,16 +7,15 @@ public class Run {
 	public final static int FAMILIES = 5;
 	public final static int POPULATION = 20;
 	public final static int OFFSPRING_MAX = 20;
+	public final static String NAME_FILEPATH = "Names/Italy";
 	
 	public static void main(String[] args) {
 		Parser parser = new Parser();
 		FamilyTree tree = new FamilyTree();
 		
-		String filepath = "Names/Italy";
-		
-		parser.feedInput(filepath, MALE);
-		parser.feedInput(filepath, FEMALE);
-		parser.feedInput(filepath, LAST);
+		parser.feedInput(NAME_FILEPATH, MALE);
+		parser.feedInput(NAME_FILEPATH, FEMALE);
+		parser.feedInput(NAME_FILEPATH, LAST);
 		
 		tree.generate(FAMILIES, POPULATION, parser.getNameList(MALE), parser.getNameList(FEMALE), parser.getNameList(LAST));
 		tree.interconnect(OFFSPRING_MAX);
