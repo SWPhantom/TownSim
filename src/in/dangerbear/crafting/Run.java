7ca64fe82b4115ac6fe73dcaa21f9edd236f50ad
Zhenya Frolov <hellcomestotown@hotmail.com>
2014-02-28 20:58:53 -0800

    Starting to segment the run.java code.
    Soon to transition into unit testing.

--- src/in/dangerbear/crafting/Run.java
+++ src/in/dangerbear/crafting/Run.java
@@ -4,6 +4,7 @@ public class Run {
 	public final static int MALE = 0;
 	public final static int FEMALE = 1;
 	public final static int LAST = 2;
+	
 	public final static int FAMILIES = 80;
 	public final static int POPULATION = 2000;
 	public final static int OFFSPRING_MAX = 20;
@@ -13,12 +14,16 @@ public class Run {
 		Parser parser = new Parser();
 		FamilyTree tree = new FamilyTree();
 		
+		//Choose and parse name files.
 		parser.feedInput(NAME_FILEPATH, MALE);
 		parser.feedInput(NAME_FILEPATH, FEMALE);
 		parser.feedInput(NAME_FILEPATH, LAST);
 		
+		//Generation segment.
 		tree.generate(FAMILIES, POPULATION, parser.getNameList(MALE), parser.getNameList(FEMALE), parser.getNameList(LAST));
 		tree.interconnect(OFFSPRING_MAX);
+		
+		//Output data of generated populace.
 		tree.printNthConnections(0, 1);
 		tree.printNthConnections(0, 2);
 		tree.printNthConnections(0, 3);
@@ -29,7 +34,5 @@ public class Run {
 		tree.printNthConnections(2, 2);
 		tree.printNthConnections(2, 3);
 		
-		//tree.DEBUG_PRINT();
 	}
-
 }
