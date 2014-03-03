8b4e86d8998d4e6675ad9f842fbfd8df79f81a12
Zhenya Frolov <hellcomestotown@hotmail.com>
2014-03-02 23:41:06 -0800

    Added JGraphT libraries and made visualizations happen.
    Great for debuggin now, but it will definitely need to be modified later. Pretty ugly...

--- src/in/dangerbear/crafting/Run.java
+++ src/in/dangerbear/crafting/Run.java
@@ -1,10 +1,23 @@
 package in.dangerbear.crafting;
 
-public class Run {
+import com.mxgraph.layout.*;
+import com.mxgraph.swing.*;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+import org.jgrapht.*;
+import org.jgrapht.ext.*;
+import org.jgrapht.graph.*;
+
+public class Run extends JApplet{
+	private static final long serialVersionUID = 2202072534703043194L;
+    private static final Dimension DEFAULT_SIZE = new Dimension(530, 320);
+	private JGraphXAdapter<String, DefaultEdge> jgxAdapter;
 	
+	public static FamilyTree tree = new FamilyTree();
 	public static void main(String[] args) {
-		FamilyTree tree = new FamilyTree();
-		
 		//Generation segment.
 		tree.generate();
 		
@@ -22,7 +35,45 @@ public class Run {
 		tree.DEBUG_PRINT();
 		
 		tree.DEBUG_CONNECTIONS_PRINT(50);
+
+		//Graph segment.
+		Run applet = new Run();
+		applet.init();
 		
+		JFrame frame = new JFrame();
+		frame.getContentPane().add(applet);
+		frame.setTitle("JGraphT Adapter to JGraph Demo");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
 	}
+	
+	public void init(){
+		// create a JGraphT graph
+        ListenableGraph<String, DefaultEdge> g =
+            new ListenableDirectedGraph<String, DefaultEdge>(
+                DefaultEdge.class);
+
+        // create a visualization using JGraph, via an adapter
+        jgxAdapter = new JGraphXAdapter<String, DefaultEdge>(g);
+
+        getContentPane().add(new mxGraphComponent(jgxAdapter));
+        resize(DEFAULT_SIZE);
+
+        for(int i = 0; i < tree.humans.size(); ++i){
+        	g.addVertex(tree.humans.get(i).getFirstName());
+        }
+
+        for(int i = 0; i < tree.humans.size(); ++i){
+        	for(int j = 0; j < tree.humans.get(i).getNumChildren(); ++j){
+	        	Human a = tree.humans.get(i);
+	        	g.addEdge(a.getFirstName(), tree.humans.get(a.children.get(j)).getFirstName());
+        	}
+        }
+        
+     // positioning via jgraphx layouts
+        mxFastOrganicLayout layout = new mxFastOrganicLayout(jgxAdapter);
+        layout.execute(jgxAdapter.getDefaultParent());
+    }
 	
 }
