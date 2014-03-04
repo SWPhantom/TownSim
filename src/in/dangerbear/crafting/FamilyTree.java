200d0c37a781b7cdce2a2fa3e9b8df9592f03e39
Zhenya Frolov <hellcomestotown@hotmail.com>
2014-03-03 18:13:55 -0800

    Fleshed out comments.

--- src/in/dangerbear/crafting/FamilyTree.java
+++ src/in/dangerbear/crafting/FamilyTree.java
@@ -13,23 +13,23 @@ import java.util.Scanner;
 
 /**
  * @author SWPhantom
- *
+ * 
  */
 
-public class FamilyTree {	
+public class FamilyTree {
 ////DECLARATIONS///////////////////////////////////////////////////////////////
 	////Statics
 	public final static int MALE = 0;
 	public final static int FEMALE = 1;
 	public final static int LAST = 2;
-	
+
 	public static int FAMILIES = 80;
 	public static int POPULATION = 2000;
 	public static int MAX_OFFSPRING = 20;
 	public static int MIN_REPRODUCTIVE_AGE = 15;
 	public static int MAX_AGE = 100;
 	public static String NAME_FILEPATH = "US";
-	
+
 	////Class variables
 	ArrayList<Human> humans;
 	BitSet eligibilityList1;
@@ -38,142 +38,150 @@ public class FamilyTree {
 	ArrayList<String> lasts;
 	Random rand = new Random();
 	Parser parser;
-	
+
 ////CONSTRUCTORS///////////////////////////////////////////////////////////////
 	public FamilyTree() {
 		humans = new ArrayList<Human>();
 		parser = new Parser();
-		
+
 		readConfig();
-		//Choose and parse name files.
+		// Choose and parse name files.
 		parser.feedInput(NAME_FILEPATH, MALE);
 		parser.feedInput(NAME_FILEPATH, FEMALE);
 		parser.feedInput(NAME_FILEPATH, LAST);
-		
+
 	}
-	
+
 ////METHODS////////////////////////////////////////////////////////////////////
-	
+
 	/**
 	 * Method generate
-	 * Generates the eligible last names, generates the Humans with random
-	 * gender assignment and names.
+	 * Generates the eligible last names and the Humans
+	 * with random gender assignment and names.
 	 * 
-	 * @param families (int) The number of families to generate (last names).
-	 * @param people (int) The number of total people to generate.
-	 * @param males (ArrayList<String>) List of male first names.
-	 * @param females (ArrayList<String>) List of female first names.
-	 * @param lasts (ArrayList<String>) List of last names.
+	 * @param families
+	 *            (int) The number of families to generate (last names).
+	 * @param people
+	 *            (int) The number of total people to generate.
+	 * @param males
+	 *            (ArrayList<String>) List of male first names.
+	 * @param females
+	 *            (ArrayList<String>) List of female first names.
+	 * @param lasts
+	 *            (ArrayList<String>) List of last names.
 	 */
 	public void generate() {
 		this.males = parser.getNameList(MALE);
 		this.females = parser.getNameList(FEMALE);
 		this.lasts = parser.getNameList(LAST);
-		
+
 		eligibilityList1 = new BitSet(POPULATION);
 		String tempLasts[] = new String[FAMILIES];
 		String tempLastName;
 		String tempFirstName;
-		
-		for(int i = 0; i < FAMILIES; ++i){
+
+		for (int i = 0; i < FAMILIES; ++i) {
 			//
 			// TODO: make sure last names don't repeat
-			// 
-			//  pop used last name from the list
+			//
+			// pop used last name from the list
 			tempLasts[i] = lasts.get(rand.nextInt(lasts.size()));
 		}
 		lasts.clear();
-		
-		for(int i = 0; i < POPULATION; ++i){
+
+		for (int i = 0; i < POPULATION; ++i) {
 			tempLastName = tempLasts[rand.nextInt(tempLasts.length)];
-			if(rand.nextInt(2) == 0){//Male created
+			if (rand.nextInt(2) == 0) {// Male created
 				tempFirstName = males.get(rand.nextInt(males.size()));
 				humans.add(new Human(tempLastName, tempFirstName, MALE));
-			}else{//Female created
+			} else {// Female created
 				tempFirstName = females.get(rand.nextInt(females.size()));
 				humans.add(new Human(tempLastName, tempFirstName, FEMALE));
 			}
 		}
 		males.clear();
 		females.clear();
-		
-		//Generate the ages of the populace.
+
+		// Generate the ages of the populace.
 		ages();
 		interconnect();
 	}
-	
+
 	/**
 	 * Method interconnect
-	 * Starts attempting to create parent/child connections for every
-	 * Human. Using the value returned by helper function makeConnection,
-	 * this attempts to stop connections ASAP.
+	 * Starts attempting to create parent/child connections
+	 * for every Human. Using the value returned by helper function
+	 * makeConnection, this attempts to stop connections ASAP.
 	 * 
 	 */
 	private void interconnect() {
-		for(int i = 0; i < humans.size(); ++i){
+		for (int i = 0; i < humans.size(); ++i) {
 			Human parent = humans.get(i);
-			//Hit the lowest age. No one after this can have children. End!
-			if(parent.getAge() < MIN_REPRODUCTIVE_AGE){
+			// Hit the lowest age. No one after this can have children. End!
+			if (parent.getAge() < MIN_REPRODUCTIVE_AGE) {
 				return;
 			}
-			
-			//Set everything up to the current human's ID to false. They are all older.
+
+			// Set everything up to the current human's ID to false. They are
+			// all older.
 			eligibilityList1.clear();
 			eligibilityList1.set(i + 1, humans.size(), true);
-			
-			//Begin elimination of potential child connections.
-			for(int j = i; j < humans.size(); ++j){
+
+			// Begin elimination of potential child connections.
+			for (int j = i; j < humans.size(); ++j) {
 				Human child = humans.get(j);
-				
-				//Check for sameness.
-				if(i == j){
+
+				// Check for sameness.
+				if (i == j) {
 					eligibilityList1.set(j, false);
 				}
-				//Check for age difference.
-				if(parent.getAge() - child.getAge() < MIN_REPRODUCTIVE_AGE){
+				// Check for age difference.
+				if (parent.getAge() - child.getAge() < MIN_REPRODUCTIVE_AGE) {
 					eligibilityList1.set(j, false);
 				}
-				
-				//Check for name difference.
-				//TODO: Maybe throw in some randomness to this.
-				//	Some people can adopt?
-				if(!parent.getLastName().equals(child.getLastName())){
+
+				// Check for name difference.
+				// TODO: Maybe throw in some randomness to this.
+				// Some people can adopt?
+				if (!parent.getLastName().equals(child.getLastName())) {
 					eligibilityList1.set(j, false);
 				}
-				
-				//Check for existing parenthood.
-				if(parent.getGender() == MALE && child.getFatherID() != -1){
+
+				// Check for existing parenthood.
+				if (parent.getGender() == MALE && child.getFatherID() != -1) {
 					eligibilityList1.set(j, false);
 				}
-				if(parent.getGender() == FEMALE && child.getMotherID() != -1){
+				if (parent.getGender() == FEMALE && child.getMotherID() != -1) {
 					eligibilityList1.set(j, false);
 				}
 			}
-			
-			//If no other humans are eligible, move onto the next potential parent.
-			if(eligibilityList1.cardinality() <= 0){
+
+			// If no other humans are eligible, move onto the next potential
+			// parent.
+			if (eligibilityList1.cardinality() <= 0) {
 				continue;
 			}
-			
-			//Create the list of ids of eligible chilren.
+
+			// Create the list of ids of eligible chilren.
 			ArrayList<Integer> eligibleChildren = new ArrayList<Integer>();
-			for(int j = i; j < eligibilityList1.size(); ++j){
-				if(eligibilityList1.get(j)){
+			for (int j = i; j < eligibilityList1.size(); ++j) {
+				if (eligibilityList1.get(j)) {
 					eligibleChildren.add(j);
 				}
 			}
-			
-			//Attempt to make a random amount of connections between the current
-			//node and the eligible children nodes.
+
+			// Attempt to make a random amount of connections between the
+			// current
+			// node and the eligible children nodes.
 			int maxOffspring = rand.nextInt(MAX_OFFSPRING);
-			for(int j = 0; j < maxOffspring; ++j){
-				//There's only one eligible unit left! Connect and stop.
-				if(eligibleChildren.size() == 1){
+			for (int j = 0; j < maxOffspring; ++j) {
+				// There's only one eligible unit left! Connect and stop.
+				if (eligibleChildren.size() == 1) {
 					makeConnection(i, eligibleChildren.get(0));
 					break;
 				}
-				
-				//Connect the ith person and someone from their eligible list.
+
+				// Connect the ith person and someone from their eligible list.
 				int index = rand.nextInt(eligibleChildren.size());
 				makeConnection(i, eligibleChildren.get(index));
 				eligibleChildren.remove(index);
@@ -181,229 +189,256 @@ public class FamilyTree {
 		}
 	}
 
-	
 	/**
 	 * Method nthConnections
-	 * Returns a list of IDs that correspond to the target Human's N linked
-	 * Human Nodes.
-	 * ie. If you call this function on a child with two parents and no kids,
-	 * the returned list will contain only his parents for N = 1.
-	 * If you call the same function with N = 2, you will get a list of the
-	 * child's parents AND grandparents.
+	 * Returns a list of IDs that correspond to the target
+	 * Human's N linked Human Nodes. ie. If you call this function on a child
+	 * with two parents and no kids, the returned list will contain only his
+	 * parents for N = 1. If you call the same function with N = 2, you will get
+	 * a list of the child's parents AND grandparents.
 	 * 
 	 * @param humanID
 	 * @return
 	 */
-	public ArrayList<Integer> nthConnections(int humanID, int n){
+	public ArrayList<Integer> nthConnections(int humanID, int n) {
 		eligibilityList1.clear();
 		ArrayList<Integer> output = new ArrayList<Integer>();
-		
+
 		nthConnectionsHelper(humanID, n);
-		
-		
-		for(int i = 0; i < humans.size(); ++i){
-			if(eligibilityList1.get(i)){
+
+		for (int i = 0; i < humans.size(); ++i) {
+			if (eligibilityList1.get(i)) {
 				output.add(i);
 			}
 		}
 		return output;
 	}
-	
+
 	/**
 	 * Method printNthConnections
-	 * 
+	 * Printing function wrapper for getNthConnection.
 	 * 
 	 * @param humanID
 	 * @param n
 	 */
-	public void printNthConnections(int humanID, int n){
-		System.out.println("Printing " + humans.get(humanID).getName() + "'s " + n + "connections.");
+	public void printNthConnections(int humanID, int n) {
+		System.out.println("Printing " + humans.get(humanID).getName() + "'s "
+				+ n + "connections.");
 		ArrayList<Integer> outputList = this.nthConnections(humanID, n);
-		
-		for(Integer i: outputList){
+
+		for (Integer i : outputList) {
 			System.out.println(humans.get(i).toString());
 		}
 		System.out.println();
 	}
-	
+
 	/**
 	 * Method hasPath
+	 * The method that initializes the eligibility BitSet and calls the
+	 * recursive hasPathHelper function.
 	 * 
-	 * 
-	 * @param source
+	 * @param source 
 	 * @param target
 	 * @return
 	 */
-	public boolean hasPath(int source, int target){
+	public boolean hasPath(int source, int target) {
 		eligibilityList1.clear();
 		return hasPathHelper(source, target);
 	}
-	
-	
+
 	/**
 	 * Method readConfig
-	 * 
+	 * Simple routine to read the Genetics config files.
+	 * Populates generational variables.
 	 * 
 	 */
-	public static void readConfig(){
+	public static void readConfig() {
 		File file = new File("Configs/Genetics.ini");
-		try{
+		try {
 			Scanner s = new Scanner(file);
-			while(s.hasNext()){
+			while (s.hasNext()) {
 				String inputToken = s.next();
-				switch(inputToken){
-				case "POPULATION:": POPULATION = s.nextInt(); break;
-	            case "FAMILIES:": FAMILIES = s.nextInt(); break;
-	            case "MAX_OFFSPRING:": MAX_OFFSPRING = s.nextInt(); break;
-	            case "MAX_AGE:": MAX_AGE = s.nextInt(); break;
-	            case "NAME_FILEPATH:": NAME_FILEPATH = s.next(); break;
-	            case "MIN_REPRODUCTIVE_AGE:": MIN_REPRODUCTIVE_AGE = s.nextInt(); break;
-	            default: break;
+				switch (inputToken) {
+				case "POPULATION:":
+					POPULATION = s.nextInt();
+					break;
+				case "FAMILIES:":
+					FAMILIES = s.nextInt();
+					break;
+				case "MAX_OFFSPRING:":
+					MAX_OFFSPRING = s.nextInt();
+					break;
+				case "MAX_AGE:":
+					MAX_AGE = s.nextInt();
+					break;
+				case "NAME_FILEPATH:":
+					NAME_FILEPATH = s.next();
+					break;
+				case "MIN_REPRODUCTIVE_AGE:":
+					MIN_REPRODUCTIVE_AGE = s.nextInt();
+					break;
+				default:
+					break;
 				}
 			}
 			s.close();
-		}catch(FileNotFoundException e){
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
 
-
 	////Helpers
-	
-	private boolean hasPathHelper(int source, int target){
-		//I've been here before. No good.
-		if(eligibilityList1.get(source) == true){
+
+	/**
+	 * Method hasPathHelper
+	 * Recursive method that does a depth-first search of the graph
+	 * to see if the source Human is connected to the Target Human.
+	 * 
+	 * @param source (int) Index/ID of the current node.
+	 * @param target (int) Index/ID of the node to be reached.
+	 * @return
+	 */
+	private boolean hasPathHelper(int source, int target) {
+		// I've been here before. No good.
+		if (eligibilityList1.get(source) == true) {
 			return false;
 		}
-		//Make sure not to come here again.
+		// Make sure not to come here again.
 		eligibilityList1.set(source, true);
-		
-		//If this has happened upon the target, we're done!
-		if(source == target){
+
+		// If this function has happened upon the target, we're done!
+		if (source == target) {
 			return true;
 		}
-		
-		//Start checking the parents and the children.
+
+		// Start checking the parents and the children.
 		Human a = humans.get(source);
-		if(a.getMotherID() != -1 && hasPathHelper(a.getMotherID(), target)) return true;
-		if(a.getFatherID() != -1 && hasPathHelper(a.getFatherID(), target)) return true;
-		for(int i = 0; i < a.getNumChildren(); ++i){
-			if(hasPathHelper(a.getChildren().get(i), target)) return true;
+		if (a.getMotherID() != -1 && hasPathHelper(a.getMotherID(), target))
+			return true;
+		if (a.getFatherID() != -1 && hasPathHelper(a.getFatherID(), target))
+			return true;
+		for (int i = 0; i < a.getNumChildren(); ++i) {
+			if (hasPathHelper(a.getChildren().get(i), target))
+				return true;
 		}
 		return false;
 	}
-	
+
 	/**
-	 * Method makeConnection
-	 * Connects the parent and child nodes, according to parameter IDs.
-	 * Makes sure the genders match on the parent connections.
+	 * Method makeConnection Connects the parent and child nodes, according to
+	 * parameter IDs. Makes sure the genders match on the parent connections.
 	 * 
-	 * @param parent (int) referencing the ID number of parent.
-	 * @param child (int) referencing the ID number of child.
-	 * @return (int)  0: Failure.
-	 * 				  1: Success.
+	 * @param parent
+	 *            (int) referencing the ID number of parent.
+	 * @param child
+	 *            (int) referencing the ID number of child.
+	 * @return (int) 0: Failure. 1: Success.
 	 */
 	private int makeConnection(int parent, int child) {
 		int parentGender = humans.get(parent).getGender();
-		
-		if(parentGender == MALE){
+
+		if (parentGender == MALE) {
 			humans.get(child).setFatherID(parent);
 			humans.get(parent).addChild(child);
 			return 1;
-		}else if(parent == FEMALE){
+		} else if (parent == FEMALE) {
 			humans.get(child).setMotherID(parent);
 			humans.get(parent).addChild(child);
 			return 1;
 		}
 		return 0;
 	}
-	
-	
+
 	/**
-	 * Method ages
-	 * This creates a distribution of ages and assigns them to the Human
-	 * objects.
+	 * Method ages This creates a distribution of ages and assigns them to the
+	 * Human objects. Does this in a greatest -> least order.
 	 * 
-	 * @param population (int) defines how many ages to generate.
+	 * TODO: Add distribution functions.
+	 * 
+	 * 
 	 */
-	private void ages(){
+	private void ages() {
 		int ages[] = new int[POPULATION];
-		for(int i = 0; i < POPULATION; ++i){
+		for (int i = 0; i < POPULATION; ++i) {
 			ages[i] = rand.nextInt(MAX_AGE + 1);
-			//ages[i] = (rand.nextGaussian()/3);
+			// ages[i] = (rand.nextGaussian()/3);
 		}
-		
+
 		Arrays.sort(ages);
-		
-		for(int i = 0; i < POPULATION; ++i){
+
+		//Set the first humans to the largest ages.
+		for (int i = 0; i < POPULATION; ++i) {
 			humans.get(i).setAge(ages[POPULATION - 1 - i]);
 		}
 	}
-	
+
 	/**
 	 * Method nthConnectionsHelper
-	 * 
+	 * Recursive function that sets the Eligibility BitSet indeces that
+	 * correspond to the Humans that are within n links from a given
+	 * Human.
 	 * 
 	 * @param humanID
 	 * @param n
 	 */
-	private void nthConnectionsHelper(int humanID, int n){
-		//If this Human has been accessed already, stop!
-		if(eligibilityList1.get(humanID)){
+	private void nthConnectionsHelper(int humanID, int n) {
+		// If this Human has been accessed already, stop!
+		if (eligibilityList1.get(humanID)) {
 			return;
 		}
-		
-		//Add yourself to the list.
+
+		// Add yourself to the list.
 		eligibilityList1.set(humanID);
-		
-		//If you're the last connection to be checked, stop before querying your connections.
-		if(n == 0){
+
+		// If you're the last connection to be checked, stop before querying
+		// your connections.
+		if (n == 0) {
 			return;
 		}
-		
-		//Stores the potential familiy to check.
+
+		// Stores the potential family to check.
 		ArrayList<Integer> payload = new ArrayList<Integer>();
-		
-		//Get the parents.
+
+		// Get the parents.
 		int temp = humans.get(humanID).getFatherID();
-		if(temp != -1) payload.add(temp);
+		if (temp != -1)
+			payload.add(temp);
 		temp = humans.get(humanID).getMotherID();
-		if(temp != -1) payload.add(temp);
-		
-		//Time to add the children. Create a temp ArrayList.
+		if (temp != -1)
+			payload.add(temp);
+
+		// Time to add the children. Create a temp ArrayList.
 		ArrayList<Integer> tempChildren = humans.get(humanID).getChildren();
-		for(int i = 0; i < tempChildren.size(); ++i){
+		for (int i = 0; i < tempChildren.size(); ++i) {
 			payload.add(tempChildren.get(i));
 		}
-		
-		for(int i = 0; i < payload.size(); ++i){
+
+		for (int i = 0; i < payload.size(); ++i) {
 			nthConnectionsHelper(payload.get(i), n - 1);
 		}
 	}
 
 	////Debug
-	
+
 	/**
-	 * Method displayMemory
-	 * Shows off the memory used at the moment.
+	 * Method displayMemory Shows off the memory used at the moment.
 	 * 
 	 */
-	public static void displayMemory(){
-	    Runtime r=Runtime.getRuntime();
-	    r.gc();
-	    r.gc();
-	    System.out.println("Memory Used="+(r.totalMemory()-r.freeMemory()));
+	public static void displayMemory() {
+		Runtime r = Runtime.getRuntime();
+		r.gc();
+		r.gc();
+		System.out.println("Memory Used=" + (r.totalMemory() - r.freeMemory()));
 	}
-	
+
 	/**
-	 * Method DEBUG_PRINT
-	 * Current desired output:
+	 * Method DEBUG_PRINT Current desired output:
 	 * 
 	 * Martinella Stranbii(99)
-	 *
+	 * 
 	 * Brighinzone Stranbii(90)
 	 *  Children:
-  	 *   Baccone de Calce
+	 *   Baccone de Calce
 	 *   Giollius de Calce
 	 *   Giovanna de Calce
 	 *   Cambius Simonis
@@ -412,29 +447,39 @@ public class FamilyTree {
 	 * Cambius Simonis(88)
 	 */
 	public void DEBUG_PRINT() {
-		for(int i = 0; i < humans.size(); ++i){
-			System.out.println(humans.get(i).getName() + "(" + humans.get(i).getAge() + ")");
-			if(humans.get(i).getNumChildren() > 0){
+		for (int i = 0; i < humans.size(); ++i) {
+			System.out.println(humans.get(i).getName() + "("
+					+ humans.get(i).getAge() + ")");
+			if (humans.get(i).getNumChildren() > 0) {
 				System.out.println(" Children:");
-				for(int j = 0; j < humans.get(i).getNumChildren(); ++j){
-					System.out.println("  " + humans.get(humans.get(i).children.get(j)).getName());
+				for (int j = 0; j < humans.get(i).getNumChildren(); ++j) {
+					System.out.println("  "
+							+ humans.get(humans.get(i).children.get(j))
+									.getName());
 				}
 			}
 			System.out.println();
 		}
 	}
-	
-	public void DEBUG_CONNECTIONS_PRINT(int tests){
+
+	/**
+	 * Method DEBUG_CONNECTIONS_PRINT
+	 * Picks two random nodes and runs hasPath on them. Prints the result.
+	 * 
+	 * @param tests
+	 */
+	public void DEBUG_CONNECTIONS_PRINT(int tests) {
 		System.out.println("Making many random connection queries:");
 		int a;
 		int b;
-		for(int i = 0; i < tests; ++i){
+		for (int i = 0; i < tests; ++i) {
 			a = rand.nextInt(humans.size());
 			b = rand.nextInt(humans.size());
-			System.out.print("Are " + humans.get(a).getName() + " and " + humans.get(b).getName() + " connected? ");
-			if(hasPath(a, b)){
+			System.out.print("Are " + humans.get(a).getName() + " and "
+					+ humans.get(b).getName() + " connected? ");
+			if (hasPath(a, b)) {
 				System.out.println("Yes");
-			}else{
+			} else {
 				System.out.println("No");
 			}
 		}
--- src/in/dangerbear/crafting/Run.java
+++ src/in/dangerbear/crafting/Run.java
@@ -11,17 +11,18 @@ import org.jgrapht.*;
 import org.jgrapht.ext.*;
 import org.jgrapht.graph.*;
 
-public class Run extends JApplet{
+public class Run extends JApplet {
 	private static final long serialVersionUID = 2202072534703043194L;
-    private static final Dimension DEFAULT_SIZE = new Dimension(530, 320);
+	private static final Dimension DEFAULT_SIZE = new Dimension(530, 320);
 	private JGraphXAdapter<String, DefaultEdge> jgxAdapter;
-	
+
 	public static FamilyTree tree = new FamilyTree();
+
 	public static void main(String[] args) {
-		//Generation segment.
+		// Generation segment.
 		tree.generate();
-		
-		//Output data of generated populace.
+
+		// Output data of generated populace.
 		tree.printNthConnections(0, 1);
 		tree.printNthConnections(0, 2);
 		tree.printNthConnections(0, 3);
@@ -31,15 +32,15 @@ public class Run extends JApplet{
 		tree.printNthConnections(2, 1);
 		tree.printNthConnections(2, 2);
 		tree.printNthConnections(2, 3);
-		
+
 		tree.DEBUG_PRINT();
-		
+
 		tree.DEBUG_CONNECTIONS_PRINT(50);
 
-		//Graph segment.
+		// Graph segment.
 		Run applet = new Run();
 		applet.init();
-		
+
 		JFrame frame = new JFrame();
 		frame.getContentPane().add(applet);
 		frame.setTitle("JGraphT Adapter to JGraph Demo");
@@ -47,33 +48,33 @@ public class Run extends JApplet{
 		frame.pack();
 		frame.setVisible(true);
 	}
-	
-	public void init(){
+
+	public void init() {
 		// create a JGraphT graph
-        ListenableGraph<String, DefaultEdge> g =
-            new ListenableDirectedGraph<String, DefaultEdge>(
-                DefaultEdge.class);
+		ListenableGraph<String, DefaultEdge> g = new ListenableDirectedGraph<String, DefaultEdge>(
+				DefaultEdge.class);
 
-        // create a visualization using JGraph, via an adapter
-        jgxAdapter = new JGraphXAdapter<String, DefaultEdge>(g);
+		// create a visualization using JGraph, via an adapter
+		jgxAdapter = new JGraphXAdapter<String, DefaultEdge>(g);
 
-        getContentPane().add(new mxGraphComponent(jgxAdapter));
-        resize(DEFAULT_SIZE);
+		getContentPane().add(new mxGraphComponent(jgxAdapter));
+		resize(DEFAULT_SIZE);
+		
+		for (int i = 0; i < tree.humans.size(); ++i) {
+			g.addVertex(tree.humans.get(i).getFirstName());
+		}
 
-        for(int i = 0; i < tree.humans.size(); ++i){
-        	g.addVertex(tree.humans.get(i).getFirstName());
-        }
+		for (int i = 0; i < tree.humans.size(); ++i) {
+			for (int j = 0; j < tree.humans.get(i).getNumChildren(); ++j) {
+				Human a = tree.humans.get(i);
+				g.addEdge(a.getFirstName(), tree.humans.get(a.children.get(j))
+						.getFirstName());
+			}
+		}
 
-        for(int i = 0; i < tree.humans.size(); ++i){
-        	for(int j = 0; j < tree.humans.get(i).getNumChildren(); ++j){
-	        	Human a = tree.humans.get(i);
-	        	g.addEdge(a.getFirstName(), tree.humans.get(a.children.get(j)).getFirstName());
-        	}
-        }
-        
-     // positioning via jgraphx layouts
-        mxFastOrganicLayout layout = new mxFastOrganicLayout(jgxAdapter);
-        layout.execute(jgxAdapter.getDefaultParent());
-    }
-	
+		// positioning via jgraphx layouts
+		mxFastOrganicLayout layout = new mxFastOrganicLayout(jgxAdapter);
+		layout.execute(jgxAdapter.getDefaultParent());
+	}
+
 }
